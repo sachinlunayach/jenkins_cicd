c0b285a5a79e59e0c9b90845f7c815126c4ec58c
sachinlunayach <sachinlunayach333@gmail.com>
2025-07-08 00:50:12 +0530

    first

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "<h1> Hello,this is the latest version of the Flask app!</h1>
+if __name__ =='__main__':
+    app.run(host='0.0.0.0',port=5000)
