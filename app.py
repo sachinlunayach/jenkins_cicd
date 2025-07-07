243b3b70a6c2ddc13ef645778263160049087c69
sachinlunayach333 <124507313+sachinlunayach@users.noreply.github.com>
2025-07-08 01:08:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return "<h1> Hello,this is the latest version of the Flask app!</h1>
-if __name__ =='__main__':
-    app.run(host='0.0.0.0',port=5000)
+    return "<h1> Hello, this is the latest version of the Flask app!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
