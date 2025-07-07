030d6f7913fd9caba5ea9e87bcc06a153bed87b1
sachinlunayach333 <124507313+sachinlunayach@users.noreply.github.com>
2025-07-08 02:21:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1> Hello,  Sachin Choudhary Jaat</h1>"
+    return "<h1> Hello,  Sachin Choudhary </h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
